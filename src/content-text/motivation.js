645f36a6980653ed85811fb4032533c5a781ebd6
mart8ins <mart8ins@gmail.com>
2022-06-02 12:48:15 +0300

    finished app

--- src/content-text/motivation.js
+++ src/content-text/motivation.js
@@ -1,77 +1,21 @@
 export const motivation = {
     lv: {
         title: "Motivācija",
-        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-        tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-        quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-        sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-        recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
-        minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
-        quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
-        fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
-        consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-        doloremque. Quaerat provident commodi consectetur veniam similique ad `,
+        text: `Visa mana līdzšinējā darba pieredze ir saistīta ar tirdzniecību. Kādu laiku es jutu sevī izaugsmi, redzēju mērķi uz kuru tiekties, bet pienāca brīdi, kas sapratu, ka esmu sasniedzis visu, ko varēju sasniegt esošajā dzīves posmā un darbā, ko daru. Sāku izjust lielu vēlmi pēc jaunu zināšanu iegūšanas un sevis tālāku attīstīšanu, kas man sniegtu arī iespēju mainīt nozari, kurā es varētu strādāt. Atrast virzienu, kurā doties nebija grūti. Mūsdienās internets un tehnoloģijas aizņem tik ļoti daudz mūsu ikdienas dzīves, ka izjutu vēlmi to iepazīt no otras puses, izstrādātāju puses. Tas aizrāva uzreiz un sapratu, ka vēlos veltīt visu savu brīvo laiku un enerģiju programmēšanas apguvei. Uz doto brīdi esmu apguvis pietiekami daudz un guvis labu pieredzi WEB aplikāciju izstrādē, lai būtu pārliecināts, ka varu uzsākt strādāt par programmētāju. Visvairāk man patīk, ka nozare un tehnoloģijas nemitīgi attīstās, kas reizēm gan var radīt sajūtu, ka nekam netiek līdzi, bet pārsvarā tas ir pozitīvi, jo ir kur sevi attīstīt un iespējams nospraust jaunus tālākus mērķus.`,
     },
     eng: {
         title: "Motivation",
-        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-        tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-        quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-        sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-        recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
-        minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
-        quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
-        fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
-        consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-        doloremque. Quaerat provident commodi consectetur veniam similique ad `,
+        text: `All my work experience so far has been in sales. For a while I felt growth in myself, saw a goal to go for, but the moment came that I realized that I had achieved everything I could achieve in the current stage of my life and the work I was doing. I began to feel a great desire to acquire new knowledge and further develop myself, which would also give me the opportunity to change the industry in which I could work. It was not difficult to find a direction to go. Nowadays, the Internet and technology take up so much of our daily lives that I felt the desire to get to know it from the other side, the developers. It immediately captivated me and I realized that I want to dedicate all my free time and energy to learn programming. At the moment I have learned enough and gained good experience in the development of WEB applications to be sure that I can start working as a programmer. What I like most is that the industry and technology are constantly evolving, which can sometimes give the impression that cant catch up to that, but mostly it is positive, because there is room for self-development and it is possible to set new goals for the future.`,
     },
 };
 
 export const goals = {
     lv: {
         title: "Mērķi",
-        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-        tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-        quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-        sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-        recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
-        minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
-        quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
-        fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
-        consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-        doloremque. Quaerat provident commodi consectetur veniam similique ad `,
+        text: `Uz šo brīdi viens no maniem galvenajiem uzdevumiem ir turpināt nostiprināt savas esošās apgūtās tehnoloģijas un zinašanas, un ik pa laikam iekļaut kādu jaunu tehnoloģiju, ko iemācīties un pielietot turpmākajos mācību projektos. Paralēli visam mācību laikam aktīvi cenšos sūtīt pieteikumus uz vakancēm ar mērķi sasniegt savu lielāko mērķi, programmētāja darbu. Es apzinos to, ka man nav darba pieredze tapēc arī saprotu, ka darba gaitas man būtu jāsāk no zemākajām pozīcijām un es esmu tam gatavs. Uz šo brīdi man svarīgākais ir, lai kāds mani pamana un dod iespēju, bet tālāk vis ir atkarīgs tikai no manis. Esmu pārliecināts, ka varu strādāt šajā sfērā, un pats galvenais, tas man ļoti patīk un jau ir kļuvis par daļu no mana dzīvesveida.`,
     },
     eng: {
         title: "Goals",
-        text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-        molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-        numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-        optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-        obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-        nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-        tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-        quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-        sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-        recusandae alias error harum maxime adipisci amet laborum. Perspiciatis 
-        minima nesciunt dolorem! Officiis iure rerum voluptates a cumque velit 
-        quibusdam sed amet tempora. Sit laborum ab, eius fugit doloribus tenetur 
-        fugiat, temporibus enim commodi iusto libero magni deleniti quod quam 
-        consequuntur! Commodi minima excepturi repudiandae velit hic maxime
-        doloremque. Quaerat provident commodi consectetur veniam similique ad `,
+        text: `At the moment one of my main tasks is to continue to strengthen my existing acquired technologies and knowledge, and time to time to learn some new technologies and apply them in future training projects. In parallel with the whole study time, I actively try to send applications for vacancies in order to achieve my biggest goal, the work of a programmer. I am aware that I do not have work experience, so I also understand that I should start my career from the lowest position and I am ready for it. At the moment, the most important thing for me is for someone to notice me and give me a chance, after that everything is in my hands to prove myself. I am convinced that I can work in this field, and most importantly, I really like it and it has already become a part of my lifestyle.`,
     },
 };
