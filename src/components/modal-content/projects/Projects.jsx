cdda61e8b2ea154c4916299c9803ce078d1aa1fd
mart8ins <mart8ins@gmail.com>
2022-06-02 14:36:56 +0300

    added feature to the projects navigation bar

--- src/components/modal-content/projects/Projects.jsx
+++ src/components/modal-content/projects/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import "./projects.css";
 import { AppContext } from "../../../context/AppContext";
 import { projectsIntro, projects } from "../../../content-text/projects";
@@ -6,13 +6,18 @@ import { projectsIntro, projects } from "../../../content-text/projects";
 const Projects = () => {
     const { eng } = useContext(AppContext);
     const projectsRef = useRef();
+    const navRef = useRef();
+    const [activeNavBtn, setActiveNavBtn] = useState("");
 
     const handleScroll = () => {
         projectsRef.current.childNodes.forEach((el) => {
             const element = el.getBoundingClientRect();
             const elementPart = element.height / 2 + 150;
-            const elementFromTop = element.top;
-            if (elementPart >= elementFromTop) {
+            const elementTopFromTop = element.top;
+            if (elementPart >= elementTopFromTop) {
+                if (elementTopFromTop < elementPart) {
+                    setActiveNavBtn(el.id);
+                }
                 el.children[0].classList.add("element__scrolled");
             }
             if (element.height <= element.top) {
@@ -25,6 +30,19 @@ const Projects = () => {
     };
 
     useEffect(() => {
+        if (navRef.current) {
+            navRef.current.childNodes.forEach((nav) => {
+                const navId = nav.id.slice(1);
+                if (navId && navId === activeNavBtn) {
+                    nav.classList.add("activeNavBtn");
+                } else {
+                    nav.classList.remove("activeNavBtn");
+                }
+            });
+        }
+    }, [activeNavBtn]);
+
+    useEffect(() => {
         if (projectsRef.current) {
             projectsRef.current.parentNode.addEventListener("scroll", handleScroll);
         }
@@ -32,10 +50,14 @@ const Projects = () => {
 
     return (
         <div className="projects__container" ref={projectsRef}>
-            <div className="project__nav">
+            <div className="project__nav" ref={navRef}>
                 <h3>{eng ? "All projects" : "Visi projekti"}</h3>
                 {projects.map((project) => {
-                    return <a href={`#${project.id}`}>{eng ? project.title.eng : project.title.lv}</a>;
+                    return (
+                        <a className={`navBtn`} key={`#${project.id}`} id={`#${project.id}`} href={`#${project.id}`}>
+                            {eng ? project.title.eng : project.title.lv}
+                        </a>
+                    );
                 })}
             </div>
             <div className="scroll__container">
@@ -47,14 +69,18 @@ const Projects = () => {
 
             {projects.map((project) => {
                 return (
-                    <div id={`${project.id}`} className="scroll__container">
+                    <div id={`${project.id}`} key={`${project.id}`} className="scroll__container">
                         <div className={`scroll-element element__hidden project__data`}>
                             <h1>{eng ? project.title.eng : project.title.lv}</h1>
                             <p>{eng ? project.about.eng : project.about.lv}</p>
 
                             <div className="tech__stack">
-                                {project.technologies.map((tech) => {
-                                    return <div className="tech__item">{tech}</div>;
+                                {project.technologies.map((tech, i) => {
+                                    return (
+                                        <div key={i} className="tech__item">
+                                            {tech}
+                                        </div>
+                                    );
                                 })}
                             </div>
 
